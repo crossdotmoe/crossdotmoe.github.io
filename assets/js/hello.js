667a8b8a66b2974e00cbed87355531e8446dabe9
crossface001 <crossface360@gmail.com>
2021-07-02 20:43:44 +0530

    Add files via upload

---
+++ assets/js/hello.js
@@ -0,0 +1,38 @@
+const hello = (() => {
+  const start = () => {
+    const hellos = [
+      { lang: "english", word: "Hello", dir: "ltr" },
+      { lang: "hebrew", word: "אהלן", dir: "rtl" },
+      { lang: "german", word: "Hallo", dir: "ltr" },
+      { lang: "spanish", word: "Hola", dir: "ltr" },
+      { lang: "esperanto", word: "Saluton", dir: "ltr" },
+    ];
+
+    let current = 0;
+
+    const createElement = (hello) => {
+      return `<h3 class="${hello.lang}" dir="${hello.dir}">${hello.word}</h3>`;
+    };
+
+    const showNext = () => {
+      if (current >= hellos.length) {
+        current = 0;
+      }
+
+      const $greeting = lib.$(".typewriter .greetings");
+      $greeting.innerHTML = createElement(hellos[current]);
+      current += 1;
+    };
+
+    setTimeout(() => {
+      showNext();
+      setInterval(showNext, 3500);
+    }, 500);
+  };
+
+  return {
+    start: start,
+  };
+})();
+
+lib.onReady(hello.start);
